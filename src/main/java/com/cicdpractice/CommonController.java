85487da02b431ed75cbe7d018c580b799afee8d6
sohee <sohee.lee@rowan.kr>
2025-07-13 10:32:39 +0900

    refactor : 헬스 체크 > 버전 조회

--- src/main/java/com/cicdpractice/CommonController.java
+++ src/main/java/com/cicdpractice/CommonController.java
@@ -1,11 +1,13 @@
 package com.cicdpractice;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +15,10 @@ public class CommonController {
 
     @GetMapping("/info/version")
     String getVersion() throws IOException {
-        return LocalDateTime.now().toString();
+        File file = new File(
+                        Objects.requireNonNull(this.getClass().getResource("/version")).getPath()
+        );
+        return FileUtils.readFileToString(file, "UTF-8");
     }
 
 }
